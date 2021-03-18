e5e5bd1b7c16ee085aae156a5b7a379876cac76e
Fernando Saibene <fsaibene@axoft.com>
2021-03-18 20:57:13 -0300

    !ejercicio parte 2 clase 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
