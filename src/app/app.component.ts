0e082089ff8dedb20202488cd5895349a0e4aa62
Fernando Saibene <fsaibene@axoft.com>
2021-03-18 21:43:08 -0300

    mejora enteros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'mi-proyecto';
-  public age1 = 0;
-  public age2 = 0;
+  public age1: number = 0;
+  public age2: number = 0;
   public average: number = 0;
   public showResult: boolean = false;
 
   public calculate(): void {
-    this.average = (+this.age1 + +this.age2) / 2;
+    this.average = (this.age1 + this.age2) / 2;
     this.showResult = true;
   }
   
